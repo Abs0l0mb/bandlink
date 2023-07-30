5a0761ca03dd63b8ee426a41e3b8caafab65d28a
Eldnir <matteo.simonet1@gmail.com>
2023-07-30 16:08:05 +0200

    sign up page

--- bandlink/pages/index.vue
+++ bandlink/pages/index.vue
@@ -1,26 +1,58 @@
 <template>
-    <div class="flex gap-10 mb-10 pt-40 w-9/12 px-10">
-        <div class="flex flex-col">
-            <h1 class="text-7xl font-semibold">
-                Bandlink
-            </h1>
-            <p class="border-t font-semibold ml-10 mb-8 border-slate-800 text-slate-800 w-80 px-4">
-                Where music comes alive
-            </p>
-            <p class="text-xl mb-8">
-                Connect with like-minded musicians effortlessly. Create bands, jam sessions, and collaborations with ease. Showcase your talent and find the perfect musical fit. Join the vibrant community of passionate musicians and unlock your true potential. BandLink - Where music comes alive</p>
-            <NuxtLink to="/account" class="bg-black hover:bg-slate-800 text-white font-bold py-2 px-4 rounded-full w-max border-4 border-violet-500 cursor-pointer duration-300">Get Started</NuxtLink>
-        </div>
-    </div>
+	<section class="w-full bg-white tails-selected-element">
+		<div class="mx-auto max-w-7xl">
+			<div class="flex flex-col lg:flex-row">
+				<div class="relative w-full bg-cover lg:w-6/12 xl:w-7/12 bg-gradient-to-r from-white via-white to-gray-100">
+					<div
+						class="relative flex flex-col items-center justify-center w-full h-full px-10 my-4 lg:px-16 lg:my-0">
+						<div class="flex flex-col items-start space-y-8 tracking-tight lg:max-w-3xl">
+							<div class="relative">
+								<h2 class="text-5xl font-bold text-gray-900 xl:text-6xl">
+									Bandlink
+								</h2>
+								<p class="mb-2 font-medium uppercase border-t border-black ml-8 px-8">
+									Where music comes alive
+								</p>
+							</div>
+							<p class="text-2xl text-gray-700">
+								Connect with like-minded musicians effortlessly. Create bands, jam sessions, and
+								collaborations with ease. Showcase your talent and find the perfect musical fit. Join the
+								vibrant community of passionate musicians and unlock your true potential.&nbsp;
+							</p>
+						</div>
+					</div>
+				</div>
 
-
-</template> 
-<script setup lang="ts">
-    /*
-    definePageMeta({
-        layout: 'xxx'
-    })
-
-    by default it uses the 'default' layout
-    */ 
-</script>
+				<div class="w-full bg-white lg:w-6/12 xl:w-5/12">
+					<div class="flex flex-col items-start justify-start w-full h-full p-4 lg:p-8 xl:p-12">
+						<h4 class="w-full text-3xl font-bold">Sign up</h4>
+						<p class="text-lg text-gray-500">
+							or, if you have an account you can
+							<a href="#_" class="text-violet-500 underline" data-primary="blue-600">sign in</a>
+						</p>
+						<div class="relative w-full mt-10 space-y-8">
+							<div class="relative">
+								<label class="font-medium text-gray-900">Email</label>
+								<input type="text"
+									class="block w-full px-4 py-2 mt-2 text-lg placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50"
+									data-primary="blue-600" data-rounded="rounded-lg"
+									placeholder="Enter Your Email Address" />
+							</div>
+							<div class="relative">
+								<label class="font-medium text-gray-900">Password</label>
+								<input type="password"
+									class="block w-full px-4 py-2 mt-2 text-lg placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50"
+									data-primary="blue-600" data-rounded="rounded-lg" placeholder="Password" />
+							</div>
+							<div class="relative">
+								<a href="#_"
+									class="inline-block w-full px-5 py-4 text-lg font-medium text-center text-white transition duration-200 bg-violet-500 rounded-lg hover:bg-violet-700 ease"
+									data-primary="violet-500" data-rounded="rounded-lg">Create Account</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
