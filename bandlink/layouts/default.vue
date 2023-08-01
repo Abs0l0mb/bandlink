f9f4470be753302e84c592f08b6f8ee14a152685
Eldnir <matteo.simonet1@gmail.com>
2023-08-02 00:21:31 +0200

    bandeau

--- bandlink/layouts/default.vue
+++ bandlink/layouts/default.vue
@@ -1,37 +1,49 @@
 <template>
-    <div>
-        <header class="shadow-sm bg-black">
-            <nav class="mx-auto p-4 flex justify-between text-white">
-                <NuxtLink to="/">
-                    <p class="font-bold rounded-lg px-3 py-2">Bandlink</p>
+    <section class="border-b-2 border-gray-100 relative w-full px-8 text-gray-700 bg-white body-font tails-selected-element"
+        data-tails-scripts="//unpkg.com/alpinejs">
+        <div class="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
+            <NuxtLink to="/"
+                class="relative z-10 flex items-center w-auto text-2xl font-extrabold leading-none text-black select-none">
+                Bandlink</NuxtLink>
+            <nav
+                class="top-0 left-0 z-0 flex items-center justify-center w-full h-full py-5 -ml-0 space-x-5 text-base md:-ml-5 md:py-0 md:absolute">
+                <NuxtLink to="/Bands" class="relative font-medium text-gray-600 duration-150 hover:text-gray-900">
+                    <span class="block">Bands</span>
                 </NuxtLink>
-
-                <ul class="flex gap-14">
-                    <li class="rounded-lg px-3 py-2">
-                        <NuxtLink to="/bands">Bands</NuxtLink>
-                    </li>
-                    <li class="rounded-lg px-3 py-2">
-                        <NuxtLink to="/musicians">Musicians</NuxtLink>
-                    </li>
-                    <li class="rounded-lg px-3 py-2">
-                        <NuxtLink to="/cortexes">Map</NuxtLink>
-                    </li>
-                    <li class="rounded-lg px-3 py-2">
-                        <NuxtLink to="/neural-networks">My Bands</NuxtLink>
-                    </li>
-                </ul>
-                <NuxtLink to="/account" class="rounded-full bg-violet-500 px-4 py-2">Account</NuxtLink>
-
+                <NuxtLink to="/Musicians" class="relative font-medium text-gray-600 duration-150 hover:text-gray-900">
+                    <span class="block">My Bands</span>
+                </NuxtLink>
+                <NuxtLink to="/Map" class="relative font-medium text-gray-600 duration-150 hover:text-gray-900">
+                    <span class="block">Map</span>
+                </NuxtLink>
+                <NuxtLink to="/MyBands" class="relative font-medium text-gray-600 duration-150 hover:text-gray-900">
+                    <span class="block">My Bands</span>
+                </NuxtLink>
             </nav>
-        </header>
-    </div>
-    <div class="text-black transition-all">
-        <slot/> 
+            <div class="relative z-10 inline-flex items-center space-x-3 md:ml-5 lg:justify-end">
+                <a href="#"
+                    class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-gray-600 whitespace-no-wrap bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none"
+                    data-rounded="rounded-md"> Sign in </a>
+                <span class="inline-flex rounded-md shadow-sm">
+                    <a href="#"
+                        class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-violet-500 border border-violet-600 rounded-md shadow-sm hover:bg-violet-600"
+                        data-rounded="rounded-md" data-primary="blue-600"> Sign up </a>
+                </span>
+            </div>
+        </div>
+    </section>
+
+    <div class="text-black duration-800">
+        <slot />
     </div>
 </template>
 
 <style scoped>
-    .router-link-exact-active {
-        color: #d8b4fe;
-    }
+.router-link-exact-active {
+    color: #8b5cf6;
+}
 </style>
+
+<script setup>
+let hover = false
+</script>
