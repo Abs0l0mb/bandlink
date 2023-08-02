464ea0be3f349628b5fb00cf5481125c7933658d
Eldnir <matteo.simonet1@gmail.com>
2023-08-02 02:26:05 +0200

    popup

---
+++ bandlink/components/PopupCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+    <div class="bg-white rounded-lg p-8 shadow-lg">
+      <!-- Add your content for the popup card here -->
+      <slot>
+        let's goo !
+      </slot>
+      <!-- Add a close button or any other UI elements if needed -->
+      <button @click="$emit('close')" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg">
+        Close
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+//faire defineprops avec le type d'entrée qu'on fait (id de groupe/musicien ?)
+</script>
+
--- bandlink/pages/bands/index.vue
+++ bandlink/pages/bands/index.vue
@@ -1,34 +1,66 @@
 <template>
-	
 	<div @click="sideBar()" class="fixed h-[5vh] w-[5vh] bg-black ml-5 mt-5 rounded-lg">
-
 	</div>
 
 	<div id="sideBar" shown="false" class="mt-16 ml-5 rounded-xl bg-black w-2/12 h-[500px] p-3 fixed duration-500 hidden">
 	</div>
 
-	<div class="flex gap-10 mx-auto pb-10 pt-12 px-10">
-		<div class="flex flex-col gap-y-8">
-			<p class="w-8/12 text-5xl font-semibold">Bands</p>
+	<div class="px-20 py-5">
+		<div class="flex gap-10 mx-auto pb-10 pt-12 px-10">
+			<div class="flex flex-col gap-y-8">
+				<p class="w-8/12 text-5xl font-semibold">Bands</p>
+			</div>
 		</div>
-	</div>
 
-	<ItemCard> </ItemCard>
+		<ItemCard @click="openPopup()" class="hover:scale-[1.02] transition-all duration-400">
+		</ItemCard>
+
+		<Transition>
+			<PopupCard v-if="showPopup" @close="closePopup" class="">
+			</PopupCard>
+		</Transition>
+	</div>
 </template>
+
+
 <script setup lang="ts">
 
-	function sideBar() {
-		let sideBar = document.getElementById("sideBar");
-		
-		if(sideBar) {
-			if(sideBar.getAttribute('shown') == "false") {
-				sideBar.className = "mt-16 ml-5 rounded-xl bg-black w-2/12 h-[500px] p-3 fixed duration-500";
-				sideBar.setAttribute('shown', 'true')
-			}
-			else {
-				sideBar.className = "mt-16 ml-5 rounded-xl bg-black w-2/12 h-[500px] p-3 fixed duration-500 hidden";
-				sideBar.setAttribute('shown', 'false')
-			}
-		} 
+import PopupCard from '~/components/PopupCard.vue';
+
+let showPopup = ref(false);
+
+function openPopup() {
+	showPopup.value = true;
+	console.log(showPopup)
+}
+function closePopup() {
+	showPopup.value = false;
+}
+
+function sideBar() {
+	let sideBar = document.getElementById("sideBar");
+
+	if (sideBar) {
+		if (sideBar.getAttribute('shown') == "false") {
+			sideBar.className = "mt-16 ml-5 rounded-xl bg-black w-2/12 h-[500px] p-3 fixed duration-500";
+			sideBar.setAttribute('shown', 'true')
+		}
+		else {
+			sideBar.className = "mt-16 ml-5 rounded-xl bg-black w-2/12 h-[500px] p-3 fixed duration-500 hidden";
+			sideBar.setAttribute('shown', 'false')
+		}
 	}
+}
 </script>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+	transition: opacity 250ms ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+	opacity: 0;
+}
+</style>
