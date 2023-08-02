6a1a28857411be40ba296940464e998c1679410a
Eldnir <matteo.simonet1@gmail.com>
2023-08-02 14:26:37 +0200

    front page debut

--- bandlink/layouts/default.vue
+++ bandlink/layouts/default.vue
@@ -2,9 +2,9 @@
     <section class="border-b-2 border-gray-100 relative w-full px-8 text-gray-700 bg-white body-font tails-selected-element"
         data-tails-scripts="//unpkg.com/alpinejs">
         <div class="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
-            <NuxtLink to="/"
-                class="relative z-10 flex items-center w-auto text-2xl font-extrabold leading-none text-black select-none">
-                <img class='w-[5vw] h-[5vw]' src='../public/favicon.ico'>Bandlink</NuxtLink>
+            <NuxtLink to="/" class="relative z-10 flex items-center w-auto text-2xl leading-none text-black select-none">
+                <img class='w-[5vw] h-[5vw]' src='../public/favicon.ico'>Bandlink
+            </NuxtLink>
             <nav
                 class="top-0 left-0 z-0 flex items-center justify-center w-full h-full py-5 -ml-0 space-x-5 text-base md:-ml-5 md:py-0 md:absolute">
                 <NuxtLink to="/Bands" class="relative font-medium text-gray-600 duration-150 hover:text-gray-900">
@@ -21,7 +21,7 @@
                 </NuxtLink>
             </nav>
             <div class="relative z-10 inline-flex items-center space-x-3 md:ml-5 lg:justify-end">
-                
+
                 <NuxtLink to="/sign-in"
                     class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-gray-600 whitespace-no-wrap bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none"
                     data-rounded="rounded-md"> Sign in </NuxtLink>
@@ -41,7 +41,7 @@
 
 <style scoped>
 .router-link-exact-active {
-    font-weight: bold;
+    font-weight: normal;
 }
 </style>
 
--- bandlink/pages/index.vue
+++ bandlink/pages/index.vue
@@ -1,7 +1,43 @@
 <template>
-	<section class="w-full bg-white tails-selected-element">
-		<div class="mx-auto max-w-7xl">
-			
+	<section class="px-2 py-16 bg-gray-100 md:px-0 tails-selected-element">
+		<div class="container items-center max-w-6xl px-8 mx-auto xl:px-5">
+			<div class="flex flex-wrap items-center sm:-mx-3">
+				<div class="w-full md:w-1/2 md:px-3">
+					<div
+						class="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
+						<h1
+							class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
+							<span class="block xl:inline">Connect With Musicians </span>
+							<span class="block text-purple-500 xl:inline">Like You !</span>
+						</h1>
+						<p class="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">Blablabla phrase pour
+							dire a quel point c'est trop dar et tout</p>
+						<div class="relative flex flex-col sm:flex-row sm:space-x-4">
+							<NuxtLink to="/sign-up"
+								class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-purple-500 rounded-md sm:mb-0 hover:bg-purple-600 sm:w-auto">
+								Sign up
+							</NuxtLink>
+							<NuxtLink to="/sign-in"
+								class="flex items-center px-6 py-3 text-gray-500 bg-gray-200 rounded-md hover:bg-gray-300 hover:text-gray-600"
+								data-rounded="rounded-md">
+								Sign in
+							</NuxtLink>
+
+						</div>
+					</div>
+				</div>
+				<div class="w-full md:w-1/2">
+					<div class="w-full h-auto overflow-hidden rounded-md sm:rounded-xl border-gray-300 border-b-2 shadow-2xl"
+						data-rounded-max="rounded-full">
+						<iframe width="560" height="315"
+							src="https://www.youtube.com/embed/6ffmvLm1yMA?autoplay=1&mute=1&controls=0&showinfo=0&modestbranding=1&loop=1&rel=0"
+							title="YouTube video player" frameborder="0"
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen>
+						</iframe>
+					</div>
+				</div>
+			</div>
 		</div>
 	</section>
 </template>
